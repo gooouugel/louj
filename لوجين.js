710865527889ca4a889c13a1e1beec8dbc478291
gooouugel <44377553+gooouugel@users.noreply.github.com>
2018-10-23 12:58:54 -0700

    Add files via upload

---
+++ لوجين.js
@@ -0,0 +1,60 @@
+﻿$("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://e.top4top.net/p_984lj2r91.jpg);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="https://f.top4top.net/p_984x7d9p1.gif">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">  </span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً 👋 ‹ <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> إدارة شات لوجين الشمري ترحب بك </div>
+      </div>
+    </div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
+if(location.protocol!='https:'){location.protocol='https:';}
+$('.break.light').css({"background-image":"url(صورة)"})
+$(`<img src=" https://d.top4top.net/p_982m6kmd1.gif" class="fr" style="width:100%;margin-right:0px;">
+<div style="text-align:center;background: #333131;">
+<a href="قريبا"><img src=https://a.top4top.net/p_9828gp391.gif" height="25" width="85" alt="شات"></a>
+<a target="_blank" href="sub.html">
+<img src=https://a.top4top.net/p_982algof1.gif" height="25" width="85"></a>
+<a target="_blank" href="قريبا على اليوتوب"><img src=https://e.top4top.net/p_982ywt6s1.gif" height="25" width="85"></a>
+<a href="javascript:void(0);" Onclick="ShowHide5();"><img src=https://f.top4top.net/p_98288ro51.gif" height="25" width="85"></a>
+</div><div style="text-align:center;color: #fff; padding: 2px; background: #7B1B11; max-width: 290px; border-radius: 12px; text-align: center; margin-right: auto; margin-left: auto; font-size:25px;  display: none;" name="help" ID="help5">
+قريبا يمكنك تواصل مع ادارة شات <br>..<a href="mailto:....."></a>
+<br>او<br>BBM: ** <br>
+<a title="شات لوجين الشمري" href="/">دردشة لوجين الشمري</a>,<a title="شات عربي" href="/">شات عربي</a>,<a title="دردشه لوجين" href="/">شات لوجين</a></div>
+<center>
+<img src=https://e.top4top.net/p_9823fhl21.gif  WIDTH=250 HEIGHT=35><br><font size="3"></font>
+<marquee direction="Up" width="100%" height="100" scrollAmount="1" align="center" style="border:49484A solid 2px;"><font size="2" color="49484A"><center>
+<br>المميزين بي شات لوجين الشمري
+<br><img src="https://a.top4top.net/p_982xdko81.gif" alt="نجم الموقع" height="30" width="100">
+<br>(<img src="https://c.top4top.net/p_9822vr981.png" alt="صورة نجم الموقع" height="30" width="100">)
+
+<br>لكـــم تحيات ادارة شات لوجين الشمري</font></marquee>
+`).insertBefore('.nav-tabs');
+$(".label-primary" ).css( "border-radius", "15px 15px 15px 15px");
+$('.nav-tabs>li>a').css('color','#7B1B11')
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="https://c.top4top.net/p_9822vr981.png" class="fl" style="margin-Right: 2px; width: 30px;">');
+$("[data-target=#wall]").attr('title','الحائط فقط للإبداع')
+$(<img style="height:50px;width: 100%;margin-right: 3px;background-color: #595A5C;" src="https://f.top4top.net/p_984gb7xn1.gif" border="0">).insertBefore('div#d2bc');
+$('<img src="https://f.top4top.net/p_982qmy4r1.gif" style="width:90px;margin-top:-90px; " class="fr">').insertBefore('.loginstat');
+$("<br>").insertBefore('.checkbox');
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet"><marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif;    height: 20px;color: #ffffff;"> مرحبا بكم في شات لوجين الشمري .. نتشرف بتواجدكم .. نقدم لكم ( مسابقات ) - ( العاب ) - ( ترفيه ) ونتمنى لكم قضاء اجمل الاوقات معنا </marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$(document).ready($("div.fr.borderg").css("display","none"));
+$('body').css({"background-image":"url()"});
+(document.getElementById("tbox").placeholder = "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ");
+$(`<center><div width="100%" style="background-color: #49484A;color: #ffffff;border-radius: 30px 30px 0px 0px;">مرحبا بكم في شات لوجين الشمري
+</div><center>`).insertBefore('#d2');function ShowHide5() { var curtable = document.getElementById("help5"); if (curtable.style.display == "none") { curtable.style.display = "block"; } else { curtable.style.display = "none"; } }
+$(`<style> .fitimg.fl.u-pic {border-radius: 50px;}.emoi {
+    width: 25px;
+    max-width: 25px;
+    height: auto;
