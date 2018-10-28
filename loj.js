1d77b617c093ac6c4070a2b2c98465a64f9e7bef
gooouugel <44377553+gooouugel@users.noreply.github.com>
2018-10-28 10:18:02 -0700

    Update loj.js

--- loj.js
+++ loj.js
@@ -1,66 +1,40 @@
-﻿$("div#tlogins button.btn.btn-primary").click(function(){
-var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
-   $(`
-    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
-      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://e.top4top.net/p_984lj2r91.jpg);" class="fl fitimg hand u-pic    ">
-      <div class="uzr fl" style="padding:0px;width:80%">
-        <div style="padding:0px;width:100%;" class="fl">
-          <img class="fl u-ico" alt="" src="https://f.top4top.net/p_984x7d9p1.gif">
-          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">  </span>
-        </div>
-        <br>
-        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
-        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً 👋 ‹ <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> إدارة شات لوجين الشمري ترحب بك </div>
-      </div>
-    </div></div>
-`).appendTo('div#d2')
-clearInterval(myVar);
+$(`<center><div>
+	<div width="99.5%" style="background-image:url('https://e.top4top.net/p_103166nnu1.gif');color: #7003300;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #70C4DE;border-top: 2px none solid#70C4DE;padding-bottom: 2px!important; width:1314px; height:24px" >
+	<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 8px 2px 10px;margin: 7px 10px 7px 10px;" color="#00527A" >مـــنورين شات لوجين </font></div></div><center>`).insertBefore('#d2');
+$(".label-primary").css("border-radius","31px 30px 30px 31px")
+$('.nav-tabs').addClass('fl').css('width','121%');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+#as {font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px; color: #00527A; border-radius: 5px 5px 5px 5px;
+    text-shadow: 2px 2px 2px rgb(138, 138, 138) , 0px 2px 10px #00527A!important;
+border: 1px solid #00527A;
+height: 33px;
+text-align: center; }
+img.fr.co{width: 16px; border-radius: 1px; transform: rotate(90deg);margin-top: 4px;}
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #00527A;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;width: 47px;height: 47px;box-shadow: 0 4px 4px -4px rgb(171,33,71), 0 0 8px rgb(171,33,71);border-radius: 40px 40px 0px 40px;}ul.nav.nav-tabs.fl li a {olor: #00527A;  box-shadow: inset 0 3px 3px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);font-family: 'jazeera',FontAwesome;font-size: 12px!important;padding: 5px!important;text-align:center!important;width: 100%;border-radius: 15px 15px 0px 0px}.nav>li>a:focus, .nav>li>a:hover {text-decoration:none;background-color: #0a1fa6;}   
+div#l1 input#u1 {border-radius: 15px 15px 0px 0px;border: 1px solid #e1e0e0;width: 99%;margin-top: 3px;margin-left: 1px;text-align: center;font-family: 'jazeera-light',FontAwesome;}
+div#l1 button {border-radius: 0px 0px 15px 15px;width: 99%;height: 30px;}
 
-}else{console.log(usmsgw)} }, 2000);
-})
+
+ul.nav.nav-tabs.fl li {border-bottom: 2px solid rgb(138, 137, 137);width: 33.3%;}textarea.fl.filw.corner.tbox {    box-shadow:inset 0 0px 0px rgb(137, 137, 137); 0 0 7px rgb(137, 137, 137);border-radius: 7px;padding-left: 5px;}
+label.label.tc.border.btn.fl {border-radius: 41px 21px 41px 21px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 1px solid #00527A;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;}
+label.fl.nosel.label.pnhead {font-family: 'jazeera',FontAwesome;width: 81%;text-align: -webkit-center;background-color: #00527A;color: white;border-radius:13px 13px 13px 13px;}
+.fr.borderg a {color: #00527A;font-size: 12px!important;}
+.fr.borderg {color: #00527A;font-size: 13px!important;border: 1px solid ##00527A;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;margin-top: 1px;}
+label.label-primary {border-radius: px 31px 31px 31px;}
+button.btn.fr.btn-success.fa.fa-refresh {padding: 4px 5px;background-color:"url()"});background-image::"url()"});border: 1px solid transparent;margin-top: -3px;border-radius: 50px;as:51px;}
 if(location.protocol!='https:'){location.protocol='https:';}
-$('.break.light').css({"background-image":"url(صورة)"})
-$(`<img src=" https://d.top4top.net/p_982m6kmd1.gif" class="fr" style="width:100%;margin-right:0px;">
-<div style="text-align:center;background: #333131;">
-<a href="قريبا"><img src=https://a.top4top.net/p_9828gp391.gif" height="25" width="85" alt="شات"></a>
-<a target="_blank" href="sub.html">
-<img src=https://a.top4top.net/p_982algof1.gif" height="25" width="85"></a>
-<a target="_blank" href="قريبا على اليوتوب"><img src=https://e.top4top.net/p_982ywt6s1.gif" height="25" width="85"></a>
-<a href="javascript:void(0);" Onclick="ShowHide5();"><img src=https://f.top4top.net/p_98288ro51.gif" height="25" width="85"></a>
-</div><div style="text-align:center;color: #fff; padding: 2px; background: #7B1B11; max-width: 290px; border-radius: 12px; text-align: center; margin-right: auto; margin-left: auto; font-size:25px;  display: none;" name="help" ID="help5">
-قريبا يمكنك تواصل مع ادارة شات <br>..<a href="mailto:....."></a>
-<br>او<br>BBM: **** <br>
-<a title="شات لوجين الشمري" href="/">دردشة لوجين الشمري</a>,<a title="شات عربي" href="/">شات عربي</a>,<a title="دردشه لوجين" href="/">شات لوجين</a></div>
-<center>
-<img src=https://e.top4top.net/p_9823fhl21.gif  WIDTH=250 HEIGHT=35><br><font size="3"></font>
-<marquee direction="Up" width="100%" height="100" scrollAmount="1" align="center" style="border:49484A solid 2px;"><font size="2" color="49484A"><center>
-<br>المميزين بي شات لوجين الشمري
-<br><img src="https://a.top4top.net/p_982xdko81.gif" alt="نجم الموقع" height="30" width="100">
-<br>(<img src="https://c.top4top.net/p_9822vr981.png" alt="صورة نجم الموقع" height="30" width="100">)
-
-<br>لكـــم تحيات ادارة شات لوجين الشمري</font></marquee>
-`).insertBefore('.nav-tabs');
-$(".label-primary" ).css( "border-radius", "15px 15px 15px 15px");
-$('.nav-tabs>li>a').css('color','#7B1B11')
-$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="https://c.top4top.net/p_9822vr981.png" class="fl" style="margin-Right: 2px; width: 30px;">');
-$("[data-target=#wall]").attr('title','الحائط فقط للإبداع')
-$(`<img style="height:50px;width: 100%;margin-right: 3px;background-color: #595A5C;" src="https://f.top4top.net/p_984gb7xn1.gif" border="0">`).insertBefore('div#d2bc');
-$('<img src="https://f.top4top.net/p_982qmy4r1.gif" style="width:90px;margin-top:-90px; " class="fr">').insertBefore('.loginstat');
-$("<br>").insertBefore('.checkbox');
-$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet"><marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif;    height: 20px;color: #ffffff;"> مرحبا بكم في شات لوجين الشمري .. نتشرف بتواجدكم .. نقدم لكم ( مسابقات ) - ( العاب ) - ( ترفيه ) ونتمنى لكم قضاء اجمل الاوقات معنا </marquee></font></b></td>
-`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
-$(document).ready($("div.fr.borderg").css("display","none"));
-$('body').css({"background-image":"url()"});
-(document.getElementById("tbox").placeholder = "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ");
-$(`<center><div width="100%" style="background-color: #49484A;color: #ffffff;border-radius: 30px 30px 0px 0px;">مرحبا بكم في شات لوجين الشمري
-</div><center>`).insertBefore('#d2');function ShowHide5() { var curtable = document.getElementById("help5"); if (curtable.style.display == "none") { curtable.style.display = "block"; } else { curtable.style.display = "none"; } }
-$(`<style> .fitimg.fl.u-pic {border-radius: 50px;}.emoi {
-    width: 25px;
-    max-width: 25px;
-    height: auto;
-    max-height: 25px;
-}</style>`).insertBefore('body');
-
-</script>
-</body>
-</html>
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #00527A;as:13px;}
+.label-primary, .btn-primary, .ae.fa.label.fa-commenting-o.label-warning, .ae.fa.chats.label.fa-comment.label-warning {    border: 1px solid ##00527A;
+    border-radius: 31px 31px 31px 31px;
+    as: 31px;
+    background-image: none;
+    border: 2px solid #10f5f2e3;
+    }
+.checkbox {color: rgb(137, 137, 137);}
+</style>`).insertBefore('body');
