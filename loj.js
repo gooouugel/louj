5d1602dbec9e047f2dd2a9a22731726fc50acf62
gooouugel <44377553+gooouugel@users.noreply.github.com>
2018-10-29 09:34:30 -0700

    Add files via upload

---
+++ loj.js
@@ -0,0 +1,31 @@
+﻿$(`<center><div><div width="99.5%" style="background-image:url(https://b.top4top.net/p_1032pbybr1.gif);color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;" ><font style="background-color: #fff;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" شات لوجين يرحب بكم </font></div></div><center>`).insertBefore('#d2');
+$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});if(location.protocol!='https:'){location.protocol='https:';} 
+$(`<link rel="stylesheet" type="text/css" href="#">
+<link rel="stylesheet" href="">
+<style>
+.border {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border: 1px solid white;}
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border-radius: 75px;}
+ul.nav.nav-tabs.fl li a {      color: #57779b;  box-shadow: inset 0 2px 2px rgb(112, 112, 113), 0 0 8px rgb(112, 112, 113);font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 3px solid rgb(112, 112, 113);width: 33.3%;}
+.label-primary, .btn-primary {border-radius: 14px 14px 14px 14px;}
+textarea.fl.filw.corner.tbox {    box-shadow:inset 0 2px 2px rgb(112, 112, 113), 0 0 8px rgb(112, 112, 113);border-radius: 7px;padding-left: 5px;}
+label.label.tc.border.btn.fl {border-radius: 40px 40px 40px 40px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 3px solid #ffffff;border-radius: 5px 5px 5px 0px;}
+label.fl.nosel.label.pnhead {width: 80%;text-align: -webkit-center;background-color: #29192b;color: white;border-radius:12px 12px 12px 12px;}
+.fr.borderg a {color: #000000;}
+.fr.borderg {color: #000000;}
+.border.corner, .border.corner {box-shadow: inset 0 1px 1px rgb(112, 112, 113), 0 0 8px rgb(112, 112, 113);border: 2px solid #ffffff;}
+.label-primary, .btn-primary {border: 1px solid #ffffff;}
+.checkbox {color: #000000;}
+</style>`).insertBefore('body');
+$(`<style>
+.fitimg.fl.u-pic {
+    box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(243, 238, 238, 0.767);
+    border-radius: 24px 0px 24px 24px;
+}
+</style>`).insertBefore('body')
+$( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
+
+
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+
